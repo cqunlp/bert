f0cb9079f3c5cce313dbf1e55c0485e30c6499d5
sugarfreeLiuYuXuan <562048502@qq.com>
2022-11-27 01:57:50 +0800

    fix fine-tuning

--- finetuningSST2/run_classify.py
+++ finetuningSST2/run_classify.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import os
 import argparse
 from tqdm import tqdm
@@ -12,7 +13,7 @@ from mindspore import dataset
 from src.config import BertConfig
 from src.optimization import BertLearningRate
 from model import BertBinaryClassificationModel
-from mindnlp.mindnlp.mindtext.dataset.classification.sst import SST2Dataset
+# from mindnlp.mindnlp.mindtext.dataset.classification.sst import SST2Dataset
 
 def getpwd():
     pwd = sys.path[0]
@@ -20,38 +21,38 @@ def getpwd():
         pwd = os.path.dirname(pwd)
     return pwd
 
-def get_sst2_dataset(dataset_path, bacth_size, max_length):
-    dataset = SST2Dataset(paths=dataset_path,
-                      tokenizer="bert-base-uncased",
-                      max_length=max_length,
-                      truncation_strategy=True,
-                      columns_list=['input_ids', 'token_type_ids', 'attention_mask', 'label'],
-                      test_columns_list=['input_ids', 'token_type_ids', 'attention_mask'],
-                      batch_size=bacth_size)
-    sst_2_ds = dataset()
-    train_dataset = sst_2_ds['train']
-    # test_dataset haven't label cause it only work for interface
-    test_dataset = sst_2_ds['dev']
-    # train_dataset_path = "{data_path}/SST2_mr/sst_2_train_data.mindrecord".format(data_path=dataset_path)
-    # test_dataset_path = "{data_path}/SST2_mr/sst_2_test_data.mindrecord".format(data_path=dataset_path)
-    # train_dataset = dataset.MindDataset(dataset_files=train_dataset_path)
-    # test_dataset = dataset.MindDataset(dataset_files=test_dataset_path)
-    # train_dataset = train_dataset.batch(bacth_size, drop_remainder=True)
-    # test_dataset = test_dataset.batch(bacth_size, drop_remainder=True)
+def get_sst2_dataset(dataset_path, train_batch_size, test_batch_size, max_length):
+    # dataset = SST2Dataset(paths=dataset_path,
+    #                   tokenizer="bert-base-uncased",
+    #                   max_length=max_length,
+    #                   truncation_strategy=True,
+    #                   columns_list=['input_ids', 'token_type_ids', 'attention_mask', 'label'],
+    #                   test_columns_list=['input_ids', 'token_type_ids', 'attention_mask'],
+    #                   batch_size=batch_size)
+    # sst_2_ds = dataset()
+    # train_dataset = sst_2_ds['train']
+    # # test_dataset haven't label cause it only work for interface
+    # test_dataset = sst_2_ds['dev']
+    train_dataset = dataset.MindDataset(dataset_files="{data_path}-{max_len}/sst_2_train_data.mindrecord".format(data_path=dataset_path, max_len=max_length))
+    test_dataset = dataset.MindDataset(dataset_files="{data_path}-{max_len}/sst_2_test_data.mindrecord".format(data_path=dataset_path, max_len=max_length))
+    train_dataset = train_dataset.batch(train_batch_size)
+    test_dataset = test_dataset.batch(test_batch_size)
     return train_dataset, test_dataset
 
 def init_sst2_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--bert_ckpt", type=str, required=True,\
                         help="Choose pretrain bert ckpt path.")
-    parser.add_argument("--dataset_path", default='SST-2', type=str,\
+    parser.add_argument("--dataset_path", default='sst-2', type=str,\
                         help="Choose dataset path.")
     parser.add_argument("--config", type=str, required=True,\
                         help="Choose bert config file.")
     # parser.add_argument("--output", default= os.path.join(getpwd(), "outputs"), type=str,\
     #                     help="Choose outputs path.")
-    parser.add_argument("--batch_size", default=16, type=int, required=True,\
-                        help="Choose batch size.")
+    parser.add_argument("--train_batch_size", default=16, type=int, required=True,\
+                        help="Choose train batch size.")
+    parser.add_argument("--test_batch_size", default=16, type=int, required=True,\
+                        help="Choose test batch size.")
     parser.add_argument("--epochs", default=10, type=int, required=True,\
                         help="Choose training epochs value.")
     parser.add_argument("--lr", default=2e-5, type=float, required=True,\
@@ -72,15 +73,19 @@ def test_loop(model,
     num_batches = dataset.get_dataset_size()
     model.set_train(False)
     total, test_loss, correct = 0, 0, 0
-    for input_ids, attention_mask, token_type_ids, label in dataset.create_tuple_iterator():
-        loss, logits = model(input_ids=input_ids,
-                        attention_mask=attention_mask,
-                        token_type_ids=token_type_ids,
-                        label=label)
-        total += len(input_ids)
-        test_loss += loss.asnumpy()
-        correct_loop = (logits.argmax(1) == label.view(-1)).asnumpy().sum()
-        correct += correct_loop
+    # 👴
+    with tqdm(total=num_batches * dataset.get_batch_size()) as t:
+        t.set_description('Evaluating')
+        for data in dataset.create_dict_iterator():
+            loss, logits = model(input_ids=data['input_ids'].squeeze(-2),
+                                 attention_mask=data['attention_mask'].squeeze(-2),
+                                 token_type_ids=data['token_type_ids'].squeeze(-2),
+                                 label=data['label'].squeeze(-2))
+            total += len(data['input_ids'])
+            test_loss += loss.asnumpy()
+            correct_loop = (logits.argmax(1) == data['label'].view(-1)).asnumpy().sum()
+            correct += correct_loop
+            t.update(dataset.get_batch_size())
     test_loss /= num_batches
     correct /= total
     # choose better accuracy ckpt
@@ -91,21 +96,35 @@ def test_loop(model,
                 epoch=current_epoch, step=current_step, acc=(correct*100)
             ))
             mindspore.save_checkpoint(model, output_ckpt_file)
-    print(f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    global best_accuracy
+    if best_accuracy <= 100*correct:
+        best_accuracy = 100*correct
+    print(f"\nTest: \n Accuracy: {(100*correct):>0.1f}%, Best-Accuracy: {best_accuracy:>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
     return 100*correct
 
 def train_and_test(model, train_dataset, dev_dataset, optimizer, current_epoch):
-    output_file = os.path.join(getpwd(), "outputs")
+    save_description = "epoch{}-max_len{}-train_bs{}-test_bs{}-lr{}-time{}".format(
+        args.epochs,
+        args.max_length,
+        args.train_batch_size,
+        args.test_batch_size,
+        args.lr,
+        time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+    )
+    output_file = os.path.join(getpwd(), "outputs", save_description)
     if not os.path.exists(output_file):
         try:
             os.mkdir(output_file)
-        except FileExistsError:
-            pass
+        except FileNotFoundError:
+            os.mkdir(os.path.join(getpwd(), "outputs"))
+            os.mkdir(output_file)
     def forward_fn(input_ids, attention_mask, token_type_ids, label):
-        loss, logits = model(input_ids=input_ids,
-                        attention_mask=attention_mask,
-                        token_type_ids=token_type_ids,
-                        label=label)
+        loss, logits = model(input_ids=input_ids.squeeze(-2),
+                        attention_mask=attention_mask.squeeze(-2),
+                        token_type_ids=token_type_ids.squeeze(-2),
+                        label=label.squeeze(-2))
         return loss, logits
     grad_fn = ops.value_and_grad(forward_fn, None, optimizer.parameters, has_aux=True)
 
@@ -119,9 +138,9 @@ def train_and_test(model, train_dataset, dev_dataset, optimizer, current_epoch):
     model.set_train(True)
 
     with tqdm(total=size * batch_size) as t:
-        for batch, (input_ids, attention_mask, token_type_ids, label) in enumerate(train_dataset.create_tuple_iterator()):
+        for batch, data in enumerate(train_dataset.create_dict_iterator()):
             t.set_description('Epoch %i' % current_epoch)
-            loss = train_step(input_ids, attention_mask, token_type_ids, label)
+            loss = train_step(data['input_ids'], data['attention_mask'], data['token_type_ids'], data['label'])
             loss, current = loss.asnumpy(), batch
             t.set_postfix(loss=loss)
             # if batch % 10 == 0:
@@ -143,22 +162,27 @@ def check_save_sst2_ckpt(test_dataset, config, sst2_ckpt):
     print('The ckpt acc is: ',acc)
 
 if __name__ == "__main__":
+    args = init_sst2_args()
+    try:
+        import zipfile
+        f = zipfile.ZipFile("{}-{}.zip".format(args.dataset_path, args.max_length))
+        f.extractall()
+        f.close()
+    except Exception:
+        raise Exception("解压数据集出错")
+
+    best_accuracy = 0
     context.set_context(mode=context.GRAPH_MODE)
     # context.set_context(mode=context.PYNATIVE_MODE)
     # train set
-    args = init_sst2_args()
+    
     epoch_num = args.epochs
     ckpt_file = args.bert_ckpt
     config = BertConfig(args.config)
     bert_sst_2 = BertBinaryClassificationModel(config, ckpt_file)
     params = bert_sst_2.trainable_params()
     # get datset
-    try:
-        train_dataset, test_dataset = get_sst2_dataset(args.dataset_path, args.batch_size, args.max_length)
-    except ModuleNotFoundError:
-        os.system('pip install -r ../requirements.txt')
-        train_dataset, test_dataset = get_sst2_dataset(args.dataset_path, args.batch_size, args.max_length)
-
+    train_dataset ,test_dataset = get_sst2_dataset(args.dataset_path, args.train_batch_size, args.test_batch_size, args.max_length)
     lr_schedule = BertLearningRate(learning_rate=args.lr,
                                    end_learning_rate=0.0,
                                    warmup_steps=int(train_dataset.get_dataset_size() * epoch_num * 0.1),
@@ -168,6 +192,6 @@ if __name__ == "__main__":
     for epoch in range(epoch_num):
         print(f"Epoch {epoch+1}\n-------------------------------")
         train_and_test(bert_sst_2, train_dataset, test_dataset, optimizer, epoch+1)
-    print("Done!")
+    print("Done! Best Accuracy is:", best_accuracy)
     # only test for fine-tuning
     # check_save_sst2_ckpt(test_dataset, config,'outputs/4_test_4_epoch_1000_step_acc_82.91284403669725%.ckpt')
